5ae812f2a6805228f93015df602569dbb54797c1
Emmanuel555 <emmanueltang7@gmial.com>
2026-04-01 18:38:23 +0800

    settled wifi, used arduino for serial monitoriing

---
+++ include/wifi_rec.h
@@ -0,0 +1,4 @@
+#pragma once // this prevents multiple inclusions
+
+void start_serial();
+void wifi_recursion();
--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,23 @@
 #include <Arduino.h>
 #include "led_test.h"
+#include "wifi_rec.h"
 
 // put function declarations here:
 int myFunction(int, int);
-String msg = "";
 
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
   est_led();
-  delay(1000);
-  Serial.begin(115200);
-  delay(1000);
-  Serial7.begin(115200);  // RX1=0, TX1=1
+  start_serial();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   light_blink();
-  while (Serial7.available()) {
-    char c = Serial7.read(); // read a byte for characters 
-
-    if (c == '\n') {
-      //Serial.println(msg);   // print full message
-      Serial7.println(msg);  // echo back full message
-      msg = "";              // reset buffer
-    } 
-    else {
-      msg += c;              // build string - formatted text
-    }
-
-    //Serial.write(c);   // echo to USB serial
-    //Serial7.write(c);  // echo back
-  }
-  
+  wifi_recursion();
 }
-
+      
 // put function definitions here:
 int myFunction(int x, int y) {
   return x + y;
